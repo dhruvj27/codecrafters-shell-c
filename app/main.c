2013aaffbef03bae0d429562c1b3371bb73851a5
dhruvj27 <dhruvjainisu213@gmail.com>
2024-12-31 10:12:52 -1000

    Implement a REPL (Read-Eval-Print Loop).

--- app/main.c
+++ app/main.c
@@ -10,12 +10,18 @@ int main() {
 
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
 
-  input[strlen(input) - 1] = '\0';
+  while (1){
+    fgets(input, 100, stdin);
 
-  printf("%s: command not found\n",input);
-  printf("$: ");
-
+    input[strlen(input) - 1] = '\0';
+    if (strcmp(input, "exit") == 0){
+      break;
+    }
+    else{
+    printf("%s: command not found\n",input);
+    printf("$ "); 
+    } 
+  }
   return 0;
 }
